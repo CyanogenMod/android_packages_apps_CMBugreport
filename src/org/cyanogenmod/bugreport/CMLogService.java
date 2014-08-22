313f84fc37827d0b98f7cd5c622f5b6e2695cbdd
Evan Widger <psychoi3oy@cyngn.com>
2014-08-22 13:19:00 -0700

    Add ART vs Dalvik label to reports
    Change-Id: Ief943b66f1052d4556c37c81240170ea527dd862

--- src/org/cyanogenmod/bugreport/CMLogService.java
+++ src/org/cyanogenmod/bugreport/CMLogService.java
@@ -53,6 +53,9 @@ public class CMLogService extends IntentService {
 
     private final static String SCRUBBED_BUG_REPORT_PREFIX = "scrubbed_";
     public static final String RO_CM_VERSION = "ro.cm.version";
+    public static final String SYSTEMLIB = "persist.sys.dalvik.vm.lib";
+    public static final String DALVIKLIB = "libdvm.so";
+    public static final String ARTLIB = "libart.so";
 
     public CMLogService() {
         super("CMLogService");
@@ -93,6 +96,12 @@ public class CMLogService extends IntentService {
                 labels.put("crash");
             } else {
                 labels.put("user");
+            }
+            String syslib = SystemProperties.get(SYSTEMLIB);
+            if (ARTLIB.equals(syslib)){
+                labels.put("ART");
+            } else if (DALVIKLIB.equals(syslib)){
+                labels.put("Dalvik");
             }
             fields.put("labels", labels);
             inputJSON.put("fields", fields);
